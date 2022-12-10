9d0c6a71df956beae848d4dffd122a24ce099c5a
Márcio Sena Santos Filho <97891534+marciosenaf@users.noreply.github.com>
2022-12-10 09:45:23 -0300

    added

--- frontend/src/components/sales/salesForm/SalesForm.js
+++ frontend/src/components/sales/salesForm/SalesForm.js
@@ -161,73 +161,73 @@ const SalesForm = ({
 }) => {
     return (
         <div className="add-sales">
-                <form className="add-sales" onSubmit={saveProduct}>
-                    <input
-                        type="text"
-                        placeholder="Product name"
-                        name="name"
-                        value={product?.name}
-                        onChange={handleInputChange}
-                    />
-                    <input
-                        type="text"
-                        placeholder="Product Category"
-                        name="category"
-                        value={product?.category}
-                        onChange={handleInputChange}
-                    />
-                    <input
-                        type="number"
-                        placeholder="Product Price"
-                        name="price"
-                        value={product?.price}
-                        onChange={handleInputChange}
-                    />
-                    <input
-                        type="number"
-                        placeholder="Product Quantity"
-                        name="Quantity"
-                        value={product?.quantity}
-                        onChange={handleInputChange}
-                    />
-                    <input
-                        type="number"
-                        placeholder="Total"
-                        name="quantity"
-                        // value={product?.quantity}
-                        onChange={handleInputChange}
-                    />
-                    <input
-                        type="number"
-                        placeholder="Amount Paid"
-                        name="quantity"
-                        // value={product?.quantity}
-                        onChange={handleInputChange}
-                    />
-                    <input
-                        type="number"
-                        placeholder="Thing"
-                        name="quantity"
-                        // value={product?.quantity}
-                        onChange={handleInputChange}
-                    />
-                    <input
-                        type="text"
-                        placeholder="Received Payment"
-                        name="quantity"
-                        // value={product?.quantity}
-                        onChange={handleInputChange}
-                    />
-                    <input
-                        type="text"
-                        placeholder="Note"
-                        name="quantity"
-                        // value={product?.quantity}
-                        onChange={handleInputChange}
-                    />
+            <form className="add-sales" onSubmit={saveProduct}>
+                <input
+                    type="text"
+                    placeholder="Product name"
+                    name="name"
+                    value={product?.name}
+                    onChange={handleInputChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Product Category"
+                    name="category"
+                    value={product?.category}
+                    onChange={handleInputChange}
+                />
+                <input
+                    type="number"
+                    placeholder="Product Price"
+                    name="price"
+                    value={product?.price}
+                    onChange={handleInputChange}
+                />
+                <input
+                    type="number"
+                    placeholder="Product Quantity"
+                    name="Quantity"
+                    // value={product?.quantity}
+                    onChange={handleInputChange}
+                />
+                <input
+                    type="number"
+                    placeholder="Total"
+                    name="quantity"
+                    // value={product?.quantity}
+                    onChange={handleInputChange}
+                />
+                <input
+                    type="number"
+                    placeholder="Amount Paid"
+                    name="quantity"
+                    // value={product?.quantity}
+                    onChange={handleInputChange}
+                />
+                <input
+                    type="number"
+                    placeholder="Received Payment"
+                    name="quantity"
+                    // value={product?.quantity}
+                    onChange={handleInputChange}
+                />
+                <input
+                    type="number"
+                    placeholder="Thing"
+                    name="quantity"
+                    // value={product?.quantity}
+                    onChange={handleInputChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Note"
+                    name="quantity"
+                    // value={product?.quantity}
+                    onChange={handleInputChange}
+                />
                 <button type="submit" className="--btn --btns-primary-sales">Save Product</button>
 
-                </form>
+            </form>
         </div>
 
     );
